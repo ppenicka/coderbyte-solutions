5075385ddd3d3c99c89dc13d764d220733815ac0
Petr Penicka <petr.penicka@gmail.com>
2020-06-10 12:23:03 +0200

    cleaned up NextPalindrome

--- NextPalindrome.js
+++ NextPalindrome.js
@@ -5,17 +5,14 @@ function NextPalindrome(num) {
 }
 
 function isPalindrome(str) {
-  if (str.length === 0) return true;
-  if (str.length === 1) return true;
+  if (str.length === 0 || str.length === 1) return true;
   if (str[0] === str[str.length-1]) {
     let substr = str.slice(1, str.length - 1);
-    console.log("isPalindrome -> substr", substr)
     return isPalindrome(substr);
   }
   return false;
 }
 
-// keep this function call here
 console.log(NextPalindrome(24));    // 33
 console.log(NextPalindrome(2));     // 3
 console.log(NextPalindrome(180));   // 181
